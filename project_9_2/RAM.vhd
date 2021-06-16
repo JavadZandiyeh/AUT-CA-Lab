3c74d31671cc55ddf762948f96d408eb4006a2e0
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2021-06-16 14:37:09 +0430

    Add files via upload

---
+++ project_9_2/RAM.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.Numeric_Std.All;
+
+entity RAM is
+	generic(
+		W : integer := 8;
+		C: integer := 5
+	);
+	
+	port(
+		WR, Clock, Reset: in std_logic;
+		Addr: in std_logic_vector(C-1 downto 0);
+		Data_in: in std_logic_vector(W-1 downto 0);
+		Data_out: out std_logic_vector(W-1 downto 0)
+	);
+end RAM;
+
+architecture Behavioral of RAM is
+	type array_of_vectors_for_ram is array(0 to (2**C)-1) of std_logic_vector(W-1 downto 0);
+	signal my_ram: array_of_vectors_for_ram;
+	
+	begin
+		prcs: process(Clock, Reset)
+		begin
+			if(Reset = '0') then
+				rst_loop: for i in 0 to (2**C)-1 loop
+					my_ram(i) <= std_logic_vector(to_unsigned(0, W));
+				end loop;
+				-- JP to address 10
+				my_ram(0) <= "01001010";
+				-- load value of address 11 in acc
+				my_ram(10) <= "00001011";
+				-- not ACC
+				my_ram(11) <= "11011111";
+				-- shr acc
+				my_ram(12) <= "11100000";
+				-- not ACC
+				my_ram(13) <= "11011111";
+				-- add ACC with value of address 15
+				my_ram(14) <= "10101111";
+				-- JPC to address 29
+				my_ram(15) <= "01111101";
+				-- and acc with address 30
+				my_ram(29) <= "10011110";
+				-- store value of ACC in address 1
+				my_ram(30) <= "00100001";
+			elsif(Clock'event and Clock = '1') then
+				if(WR = '1') then
+					my_ram(to_integer(unsigned(Addr))) <= Data_in;
+				end if;
+				Data_out <= my_ram(to_integer(unsigned(Addr)));
+			end if;
+		end process;
+		
+end Behavioral;
---
+++ project_9_2/tb_Basic_Computer.vhd
@@ -0,0 +1,65 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY tb_Basic_Computer IS
+END tb_Basic_Computer;
+ 
+ARCHITECTURE behavior OF tb_Basic_Computer IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Basic_Computer
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         PC_out : OUT  std_logic_vector(4 downto 0);
+         MAR_out : OUT  std_logic_vector(4 downto 0);
+         ACC_out : OUT  std_logic_vector(7 downto 0);
+         MDR_out : OUT  std_logic_vector(7 downto 0);
+         IR_out : OUT  std_logic_vector(7 downto 0);
+         CF_out : OUT  std_logic;
+			state_out : out std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+
+ 	--Outputs
+   signal PC_out : std_logic_vector(4 downto 0);
+   signal MAR_out : std_logic_vector(4 downto 0);
+   signal ACC_out : std_logic_vector(7 downto 0);
+   signal MDR_out : std_logic_vector(7 downto 0);
+   signal IR_out : std_logic_vector(7 downto 0);
+   signal CF_out : std_logic;
+	signal state_out : std_logic_vector(3 downto 0);
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Basic_Computer PORT MAP (
+          clk => clk,
+          rst => rst,
+          PC_out => PC_out,
+          MAR_out => MAR_out,
+          ACC_out => ACC_out,
+          MDR_out => MDR_out,
+          IR_out => IR_out,
+          CF_out => CF_out,
+			 state_out => state_out
+        );
+
+	rst <= '0', '1' after 5 ns;
+	
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for 10 ns;
+		clk <= '1';
+		wait for 10 ns;
+   end process;
+ 
+END;
