92f179056885260dda9803cdf9fce33321f07134
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2021-06-16 14:35:57 +0430

    Update and rename tb_Basic_Computer.vhd to tb_Basic_Computer1.vhd

--- project_9_1/tb_Basic_Computer1.vhd
+++ project_9_1/tb_Basic_Computer1.vhd
@@ -1,20 +1,50 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   09:49:39 06/09/2021
+-- Design Name:   
+-- Module Name:   C:/Users/aal/Xilinx/basic_Comouter/tb_basicComputer1.vhd
+-- Project Name:  basic_Comouter
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Basic_Computer
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-
  
-ENTITY tb_Basic_Computer IS
-END tb_Basic_Computer;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
  
-ARCHITECTURE behavior OF tb_Basic_Computer IS 
+ENTITY tb_basicComputer1 IS
+END tb_basicComputer1;
+ 
+ARCHITECTURE behavior OF tb_basicComputer1 IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
+ 
     COMPONENT Basic_Computer
     PORT(
          arst : IN  std_logic;
          clk : IN  std_logic;
-         ac_out : OUT  std_logic_vector(15 downto 0);
-         pc_out : OUT  std_logic_vector(3 downto 0);
-         ir_out : OUT  std_logic_vector(15 downto 0);
+         akku : INOUT  std_logic_vector(15 downto 0);
+         irReg : OUT  std_logic_vector(15 downto 0);
          ram0 : OUT  std_logic_vector(15 downto 0);
          ram1 : OUT  std_logic_vector(15 downto 0);
          ram2 : OUT  std_logic_vector(15 downto 0);
@@ -30,24 +60,7 @@ ARCHITECTURE behavior OF tb_Basic_Computer IS
          ram12 : OUT  std_logic_vector(15 downto 0);
          ram13 : OUT  std_logic_vector(15 downto 0);
          ram14 : OUT  std_logic_vector(15 downto 0);
-         ram15 : OUT  std_logic_vector(15 downto 0);
-			
-			reg0 : out std_logic_vector(15 downto 0);
-			reg1 : out std_logic_vector(15 downto 0);
-			reg2 : out std_logic_vector(15 downto 0);
-			reg3 : out std_logic_vector(15 downto 0);
-			reg4 : out std_logic_vector(15 downto 0);
-			reg5 : out std_logic_vector(15 downto 0);
-			reg6 : out std_logic_vector(15 downto 0);
-			reg7 : out std_logic_vector(15 downto 0);
-			reg8 : out std_logic_vector(15 downto 0);
-			reg9 : out std_logic_vector(15 downto 0);
-			reg10 : out std_logic_vector(15 downto 0);
-			reg11 : out std_logic_vector(15 downto 0);
-			reg12 : out std_logic_vector(15 downto 0);
-			reg13 : out std_logic_vector(15 downto 0);
-			reg14 : out std_logic_vector(15 downto 0);
-			reg15 : out std_logic_vector(15 downto 0)
+         ram15 : OUT  std_logic_vector(15 downto 0)
         );
     END COMPONENT;
     
@@ -56,10 +69,11 @@ ARCHITECTURE behavior OF tb_Basic_Computer IS
    signal arst : std_logic := '0';
    signal clk : std_logic := '0';
 
+	--BiDirs
+   signal akku : std_logic_vector(15 downto 0);
+
  	--Outputs
-   signal ac_out : std_logic_vector(15 downto 0);
-   signal pc_out : std_logic_vector(3 downto 0);
-   signal ir_out : std_logic_vector(15 downto 0);
+   signal irReg : std_logic_vector(15 downto 0);
    signal ram0 : std_logic_vector(15 downto 0);
    signal ram1 : std_logic_vector(15 downto 0);
    signal ram2 : std_logic_vector(15 downto 0);
@@ -76,24 +90,9 @@ ARCHITECTURE behavior OF tb_Basic_Computer IS
    signal ram13 : std_logic_vector(15 downto 0);
    signal ram14 : std_logic_vector(15 downto 0);
    signal ram15 : std_logic_vector(15 downto 0);
-	
-	
-	signal reg0 : std_logic_vector(15 downto 0);
-	signal reg1 : std_logic_vector(15 downto 0);
-	signal reg2 : std_logic_vector(15 downto 0);
-	signal reg3 : std_logic_vector(15 downto 0);
-	signal reg4 : std_logic_vector(15 downto 0);
-	signal reg5 : std_logic_vector(15 downto 0);
-	signal reg6 : std_logic_vector(15 downto 0);
-	signal reg7 : std_logic_vector(15 downto 0);
-	signal reg8 : std_logic_vector(15 downto 0);
-	signal reg9 : std_logic_vector(15 downto 0);
-	signal reg10 : std_logic_vector(15 downto 0);
-	signal reg11 : std_logic_vector(15 downto 0);
-	signal reg12 : std_logic_vector(15 downto 0);
-	signal reg13 : std_logic_vector(15 downto 0);
-	signal reg14 : std_logic_vector(15 downto 0);
-	signal reg15 : std_logic_vector(15 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
@@ -101,9 +100,8 @@ BEGIN
    uut: Basic_Computer PORT MAP (
           arst => arst,
           clk => clk,
-          ac_out => ac_out,
-          pc_out => pc_out,
-          ir_out => ir_out,
+          akku => akku,
+          irReg => irReg,
           ram0 => ram0,
           ram1 => ram1,
           ram2 => ram2,
@@ -119,35 +117,19 @@ BEGIN
           ram12 => ram12,
           ram13 => ram13,
           ram14 => ram14,
-          ram15 => ram15,
-			 
-			 reg0 => reg0,
-          reg1 => reg1,
-          reg2 => reg2,
-          reg3 => reg3,
-          reg4 => reg4,
-          reg5 => reg5,
-          reg6 => reg6,
-          reg7 => reg7,
-          reg8 => reg8,
-          reg9 => reg9,
-          reg10 => reg10,
-          reg11 => reg11,
-          reg12 => reg12,
-          reg13 => reg13,
-          reg14 => reg14,
-          reg15 => reg15		 
+          ram15 => ram15
         );
-
-	arst <= '0', '1' after 5 ns;
-	
+	arst<='1','0' after 5ns;
    -- Clock process definitions
    clk_process :process
    begin
 		clk <= '0';
-		wait for 10 ns;
+		wait for 50ns;
 		clk <= '1';
-		wait for 10 ns;
+		wait for 50ns;
    end process;
  
+
+ 
+
 END;
